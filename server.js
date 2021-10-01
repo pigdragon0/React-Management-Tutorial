0ec432b8d3d1e2f2be252568e326b2404c66cf88
pigdragon0 <dragonpig@nate.com>
2021-10-01 11:04:05 +0900

    디비연동

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,23 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  post: conf.port,
+  database: conf.database,
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: 'http://placeimg.com/64/64/1',
-      name: '루피',
-      birthday: '961222',
-      gender: '남자',
-      job: '해적',
-    },
-    {
-      id: 2,
-      image: 'http://placeimg.com/64/64/2',
-      name: '조로',
-      birthday: '940112',
-      gender: '남자',
-      job: '검사',
-    },
-    {
-      id: 3,
-      image: 'http://placeimg.com/64/64/3',
-      name: '나미',
-      birthday: '980522',
-      gender: '여자',
-      job: '도둑',
-    },
-  ]);
+  connection.query('SELECT * FROM CUSTOMER', (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
